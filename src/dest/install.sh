314d3389db50068ff69a65dd02e0243e849dcc80
Ricardo Padilha <ricardospadilha@gmail.com>
2015-04-15 18:00:14 -0700

    Updated scripts

--- src/dest/install.sh
+++ src/dest/install.sh
@@ -7,18 +7,17 @@ name="$(basename "${prog_dir}")"
 tmp_dir="/tmp/DroboApps/${name}"
 logfile="${tmp_dir}/install.log"
 
+# ensure tmp folder exists
+if [ ! -d "${tmp_dir}" ]; then mkdir -p "${tmp_dir}"; fi
+# redirect all output to logfile
+exec 3>&1 4>&2 1>> "${logfile}" 2>&1
+# log current date, time, and invocation parameters
+echo "$(date +"%Y-%m-%d %H-%M-%S"):" "${0}" "${@}"
 # script hardening
 set -o errexit  # exit on uncaught error code
 set -o nounset  # exit on unset variable
 set -o pipefail # propagate last error code on pipe
 set -o xtrace   # enable script tracing
-
-# ensure log folder exists
-if [ ! -d "${tmp_dir}" ]; then mkdir -p "${tmp_dir}"; fi
-# redirect all output to logfile
-exec 3>&1 4>&2 1>> "${logfile}" 2>&1
-# log current date, time, and invocation parameters
-echo "$(date +"%Y-%m-%d %H-%M-%S"): ${0}" "${@}"
 
 # copy default configuration files
 find "${prog_dir}" -type f -name "*.default" -print | while read deffile; do
